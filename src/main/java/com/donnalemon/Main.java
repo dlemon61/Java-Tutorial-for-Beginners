f38cf5400aeabe917c0438eae11ef45ac796c679
Donna Lemon <donnalemon1@gmail.com>
2022-04-19 15:25:06 -0400

    Sixth push.

--- src/main/java/com/donnalemon/Main.java
+++ src/main/java/com/donnalemon/Main.java
@@ -5,10 +5,9 @@ import java.util.Date;
 
 public class Main {
     public static void main (String args[]) {
-        Point point1 = new Point( 1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        // c:\Windows\...
+        String message = "c:\tWindows\\...";
+        System.out.println(message);
 
 
 
