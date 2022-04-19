69fed4e9b081b24431f3e20752040423ab1ff254
Donna Lemon <donnalemon1@gmail.com>
2022-04-19 14:00:12 -0400

    Fourth push.

--- src/main/java/com/donnalemon/Main.java
+++ src/main/java/com/donnalemon/Main.java
@@ -1,12 +1,14 @@
 package com.donnalemon;
 
+import java.util.Date;
+
 public class Main {
     public static void main (String args[]) {
-        byte myAge = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y);
+
 
 
     }
